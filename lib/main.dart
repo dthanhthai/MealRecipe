e3377255bee85f1e02c06fe9d202a13cc1c2ab74
Thai Doan <thai.doan@axonactive.com>
2020-05-28 19:04:02 +0700

    lesson 197

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:mealrecipe/screens/categories_screen.dart';
+import 'package:mealrecipe/screens/category_meals_screen.dart';
+import 'package:mealrecipe/screens/meal_detail_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MealRecipe',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold),
+            ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+//      home: CategoriesScreen(),
+    initialRoute: '/',
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen()
+      },
     );
   }
 }
+
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -28,7 +53,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +61,8 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Container(), // This trailing comma makes auto-formatting nicer for build methods.
+      body:
+          Container(), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
